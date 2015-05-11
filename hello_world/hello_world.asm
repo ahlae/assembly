b51d91b914db9d5c6fc059e5a89ff4f5f56082d9
Alex <ahla@mail.de>
2015-05-12 01:47:28 +0200

    Added explanatory comments to hello_world.asm

--- hello_world/hello_world.asm
+++ hello_world/hello_world.asm
@@ -1,23 +1,23 @@
-SECTION .data ; Section containing initialized data
+SECTION .data				; Section containing initialized data
 
 EatMsg: db "Eat at Joe's!",10
 EatLen: equ $-EatMsg
 
 
-SECTION .bss
-SECTION .text
+SECTION .bss				; Section containing uninitialized data
+SECTION .text				; Section containing code
 
-global _start
+global _start				; Linker needs this to find the entry point!
 
 _start:
-  nop
-  mov eax,4
-  mov ebx,1
-  mov ecx,EatMsg
-  mov edx,EatLen
-  int 80H
+  nop					; This no-op keeps gdb happy (see text)
+  mov eax,4				; Specify sys_write syscall
+  mov ebx,1				; Specify File Descriptor 1: Standard Output
+  mov ecx,EatMsg			; Pass offset of the message
+  mov edx,EatLen			; Pass the length of the message
+  int 80H				; Make syscall to output the text to stdout
 
-  mov eax,1
-  mov ebx,0
-  int 80H
+  mov eax,1				; Specify Exit syscall
+  mov ebx,0				; Return a code of zero
+  int 80H				; Make syscall to terminate the program
 
